9bfe3b8b42f282b130300ed325b5d5f9d3263857
Nourajazbeh <dr.noorj19@gmail.com>
2024-03-20 15:54:51 +0100

    datalieren service.ts

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -8,35 +8,42 @@ export class CarService {
   public getCarById(id: number): Promise<any> {
     const carId = Number(id);
     return new Promise((resolve) => {
-    const car = this.cars.find((car) => car.id ===id);
+    const car = this.cars.find((car) => car.id === carid);
     if (!car) {
       throw new HttpException('Book does not exist!', 404);
 
     }
-    car;
-  }
+    return resolve(car);
+  });
+}  
 
-  public deleteCarById(id: number).: Promise<any> {
-    const carId = Nunber(id);
+  public deleteCarById(id: number): Promise<any> {
+    const carId = Number(id);
     return new Promise((resolve) => {
-    const car = this.cars.findIndex((car) => car.id ===carId);
+    const index = this.cars.findIndex((car) => car.id ===carId);
     if (index === -1) {
        throw new HttpException('Not found', 404);
     }
-     return this.cars;
-   =}  
+    this.cars.splice(index, 1);
+     return  resolve(this.cars);
+  });  
     
   }  
 
-  public putCarById(id: number, propertyName: string, propertyValue: string) {
+  public putCarById
+  id: number,
+  propertyName: string,
+  propertyValue: string,
+  ); Promise<any> {
     const carId = Number(id);
-    return new Promise((resolve) => {}
-     const index = this.cars.findIndex((car) => car.id== id);
+    return new Promise((resolve) => {
+     const index = this.cars.findIndex((car) => car.id === carid);
      if (index == -1) { 
         throw new HttpException('Not Found', 404);
      }
      this.cars[index][propertyName] = propertyValue;
      return resolve(this.cars);
     
-    };
-}
+    });
+  }
+}  
